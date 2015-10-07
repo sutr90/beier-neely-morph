8ab146b51c50537c9a0d58e1336a83f10dd3fc9d
sutr90 <sutr90@gmail.com>
2015-10-07 20:01:03 +0200

    Removed old code.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "GetTimeMs.h"
-#include "morph_image.h"
 #include "morph_dlib.h"
 
 
@@ -10,18 +9,9 @@ using namespace std;
 int main(int argc, char *argv[]) {
     uint64 start = GetTimeMs64();
 
-    morph_image();
-
-    uint64 end = GetTimeMs64();
-
-    cout << end - start << endl;
-
-
-    start = GetTimeMs64();
-
     morph_image_dlib();
 
-    end = GetTimeMs64();
+    uint64 end = GetTimeMs64();
 
     cout << end - start;
     return 0;
